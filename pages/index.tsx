cc897a6ba76569e02b6d16c9df2841a6a641c9b7
cactusballs <lottielucyjudge@gmail.com>
2025-02-19 18:45:04 +0000

    refactor etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,84 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link'
 
-<!DOCTYPE html>
+import '../styles/global.css';
 
-<html>
-  <head>
-    <title>Lottie Judge </title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="stylesheet.css" type="text/css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-    crossorigin="anonymous"></script>
-    <script src="portfolio.js" defer> </script>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  </head>
 
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">LJ</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+const Index = () => {
+  return (
+    <>
+      <head>
+        <title>Lottie Judge</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <a className="navbar-brand" href="#">LJ</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="who/who.html">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="codingwork/codingwork.html">Web Development Work</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="CV/cv.html">Other Professional Work</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact/contact.html">Gallery</a>
-                    </li>
-                </ul>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" href="/who">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" href="/codingwork">Web Development Work</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" href="/cv">Other Professional Work</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" href="/gallery">Gallery</Link>
+                </li>
+              </ul>
             </div>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <div className="hero">
+          <h4>Hiya, I'm</h4>
+          <h1>Lottie Judge</h1>
+          <h2>and I love the internet</h2>
+          <p>Full-Stack / Python & JavaScript / Web Development / Web Design</p>
         </div>
-    </nav>
-</header>
-<main>
- <div class="hero">
-  <h4>Hiya, I'm</h4>
-  <h1>Lottie Judge</h1>
-  <h2>and I love the internet</h2>
-  <p>Full-Stack / Python & JavaScript / Web Development / Web Design /</p>
-</div> 
 
+        <div className="about">
+          <h2>About</h2>
+          <div className="wrapper">
+            <div className="box">
+              <p>
+                Skilled in Full-Stack Development. <br />
+                Proficient in: JavaScript, Python, SQL, Version Control, HTML & CSS. <br />
+                I love building things for the internet and interpreting people's IRL businesses into effective and easy-on-the-eyes websites. <br />
+                Have a look at my previous work <Link href="/codingwork">here</Link>. <br />
+                <br />
+                If you have any questions, ideas, or fancy a chat, get in touch <Link href="/contact">below</Link>.
+                <br />
+                Thanks for visiting.
+              </p>
+            </div>
+            <div className="boxPhoto">
+              <Image src="/images/profile.jpeg" alt="Lottie Judge" width={250} height={250} />
+            </div>
+          </div>
+        </div>
+      </main>
 
+      <footer>
+        <h3 className="contact">
+          <a href="mailto:lottiejudge@gmail.com">lottiejudge@gmail.com</a> |{' '}
+          <a href="https://www.linkedin.com/feed/">LinkedIn</a> |{' '}
+          <a href="https://github.com/cactusballs">GitHub</a>
+        </h3>
+      </footer>
+    </>
+  );
+};
 
-<div class="about">
-<h2>About</h2>
-<div class="wrapper">
-<div class="box">
-<p>
-  skilled in Full-Stack Development. 
-  <br>
-  Proficient in: JavaScript, Python, SQL, Version Control, HTML & CSS.
-<br>
-i love building things for the internet and interpreting people's irl businesses into effective and easy on the eyes websites. <br>
-have a look at my previous work <a href="codingwork/codingwork.html">here</a><br>
-<br>
-if you have any questions, ideas or fancy a chat get in touch <a href="contact">below</a>.
-<br>
-thanks for visiting. 
-</p>
-</div>
-<div class="boxPhoto"><img src="images/profile.jpeg">
-  </div>
-</div>
-</div>
-
-</main>
-<footer>
-  <h3 class="contact">lottiejudge@gmail.com | <a href="https://www.linkedin.com/feed/">Linkedin</a> | <a href="https://github.com/cactusballs">Github</a></>
-</footer>
-</body>
-</html>
-
+export default Index;
 
